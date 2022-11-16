7a674e44a2ccfb22876e2c2173e547b0fe34d92a
Mihai Gavrilescu <mihai.gavrilescu@yahoo.com>
2022-11-16 18:17:27 +0200

    BMS-15: Add Hibernate configuration (pom and utils).

---
+++ src/main/java/com/sda/mihai/bookmanagement/utils/SessionManager.java
@@ -0,0 +1,26 @@
+package com.sda.mihai.bookmanagement.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionManager extends AbstractSessionManager {
+
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    private SessionManager() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return INSTANCE.getSessionFactory("book_management_system");
+    }
+
+    public static void shutDown() {
+        INSTANCE.shutdownSessionManager();
+    }
+
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        // Hibernate Model will be added here
+
+    }
+}
